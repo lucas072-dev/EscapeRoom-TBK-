4b6d807379efaea7c60cfb8b63095eb34bb4ea94
Natas Pijoraitis <nataspijoraitis@gmail.com>
2026-01-16 10:01:28 +0100

    Add files via upload

--- test.py
+++ test.py
@@ -1,1 +1,18 @@
-print("Hallo wereld!")
+import time
+import sys
+
+def typewriter(text, delay=0.05):
+    for char in text:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(delay)
+    print()
+
+typewriter("password:", 0.07)
+input()
+if input == "1908":
+	typewriter("De password is correct!")
+else:
+	typewriter("De password is incorrect!")
+
+ 
